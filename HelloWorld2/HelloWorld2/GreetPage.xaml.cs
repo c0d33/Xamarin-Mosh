a4a1ebbd6fcafc32d1a227c705625808deb53734
Cody Sheridan <sheridanc@theleeco.com>
2021-03-04 14:56:48 -0500

    Stack layout

---
+++ HelloWorld2/HelloWorld2/GreetPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+
+
+            //if (Device.RuntimePlatform == Device.iOS)
+            //{
+            //    Padding = new Thickness(0, 20, 0, 0);
+            //}
+
+            //int padding = 0;
+            //switch (Device.RuntimePlatform)
+            //{
+            //    case Device.Android:
+            //        padding = 10;
+            //        break;
+            //    case Device.iOS:
+            //        padding = 20;
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            //Padding = new Thickness(0, padding, 0, 0);
+        }
+    }
+}
---
+++ HelloWorld2/HelloWorld2/QuotesPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld2
+{
+    public partial class QuotesPage : ContentPage
+    {
+        private int _index = 0;
+        private string [] _quotes = new string []
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            CurrentQuote.Text = _quotes [_index];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            _index++;
+            if (_index >= _quotes.Length)
+                _index = 0;
+            
+            CurrentQuote.Text = _quotes [_index];
+        }
+    }
+}
